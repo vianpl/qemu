cc33f06a3e6a166b9d2730c7aa5c34ca0be25967
Nicolas Saenz Julienne <nsaenz@amazon.com>
2023-10-11 16:56:13 +0000

    ENABLE_VP_VTL
    Signed-off-by: Nicolas Saenz Julienne <nsaenz@amazon.com>

--- include/hw/hyperv/hyperv.h
+++ include/hw/hyperv/hyperv.h
@@ -60,6 +60,8 @@ int hyperv_set_event_flag_handler(uint32_t conn_id, EventNotifier *notifier);
 
 uint16_t hyperv_hcall_vtl_enable_partition_vtl(CPUState *cs, uint64_t param1,
                                                uint64_t param2, bool fast);
+
+uint16_t hyperv_hcall_vtl_enable_vp_vtl(CPUState *cs, uint64_t param, bool fast);
 /*
  * Process HV_POST_MESSAGE hypercall: parse the data in the guest memory as
  * specified in @param, and call the HvMsgHandler associated with the
