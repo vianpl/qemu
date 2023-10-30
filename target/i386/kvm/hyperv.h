3efc59a46fbec99c05f7e620273bd4f7521dd842
Nicolas Saenz Julienne <nsaenz@amazon.com>
2023-10-30 11:38:33 +0000

    XXX: VSM reset prep
    VSM prep work to deal with reset signals. WIP.
    
    Signed-off-by: Nicolas Saenz Julienne <nsaenz@amazon.com>

--- target/i386/kvm/hyperv.h
+++ target/i386/kvm/hyperv.h
@@ -25,6 +25,7 @@ int kvm_hv_handle_exit(X86CPU *cpu, struct kvm_hyperv_exit *exit);
 int hyperv_x86_synic_add(X86CPU *cpu);
 void hyperv_x86_synic_reset(X86CPU *cpu);
 void hyperv_x86_synic_update(X86CPU *cpu);
+void hyperv_x86_vsm_reset(X86CPU *cpu);
 
 int hyperv_x86_vsm_init(X86CPU *cpu);
 
